bc2fad705ef61be364c7e3f8f464220e7d669240
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-10-04 16:06:01 +0530

    Add files via upload

---
+++ riya.py
@@ -0,0 +1,44 @@
+'''
+print("hello \n world")
+if 5>2:
+     print("5>2")
+     print("5>2")
+
+
+x=6
+y="ann"
+print(x,y)
+
+x=str(3)
+y=int(3)
+z=float(3)
+print(x,y,z)
+
+x=3
+y=("ann")
+print(type(x))
+print(type(y))
+
+x,y,z="organe","blue","black"
+print(x)
+print(y)
+print(z)
+
+x="hlo"
+y=" hi"
+z=" hlo"
+print(x+y+z)
+
+
+username=input("enter username: ")
+print("your username is: ",username)
+
+
+
+a="hello "
+b= "world"
+c=a+b
+print(c)
+'''
+a="hello, world"
+print(a[2:5])
