411a769ee9cce32f723bbc85f62ed2f5f1ac0eea
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-10-18 15:57:06 +0530

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,11 @@
+'''Author:Riya
+Date:18-10-2024
+Description: program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+'''
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+print("sum=",number1+number2,",Division=",number1/number2)
+print("Is number 1greater than number 2?:",number1>number2)
+print("Are number 1 and number 2 are equal:",number1==number2)
+print("Logical And:",number1>0 and number2>0)
+print("Logical Or:",number1>0 or number2>number1)
