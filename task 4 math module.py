efe54ef5e32c527edd4662b4e79118755acdf51e
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-10-18 16:24:49 +0530

    Add files via upload

---
+++ task 4 math module.py
@@ -0,0 +1,13 @@
+'''Author:Riya
+Date:18-10-2024
+Description program that uses functions from the math module to perform the following operations on a number provided by the user:'''
+
+import math
+number=int(input("Enter a number:"))
+sqaure_root=math.sqrt(number)
+print("square root of ",number,"is 5:",sqaure_root)
+factorial=math.factorial(number)
+print("factorial of ",number,":",factorial)
+power=math.pow(number,2)
+print(power)
+
