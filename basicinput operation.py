89b3bf483f4972d5a0fa69b283ca9ef4d91906a9
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-10-18 15:10:25 +0530

    Add files via upload

---
+++ basicinput operation.py
@@ -0,0 +1,8 @@
+'''Author:Riya
+Datew:22-3-24
+Description:program that asks the user to input their name and age .after receving the input ,display a message that
+ includes the users name and age.
+ Hello,john! you are 25 years old.'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Hello,",name,"! you are",age,"years old")
---
+++ task 2 string.py
@@ -0,0 +1,17 @@
+'''
+Author:Riya
+Datew:22-3-24
+Description    Assign the string "Hello, World!" to a variable.
+    Extract the substring "World" from the string.
+    Concatenate this substring with the string " Everyone!".
+    Print the final concatenated string.
+
+
+'''
+
+str_var="Hello,World!"
+str_another_var=(str_var[6:11])
+print(str_another_var)
+final_str=str_another_var+(" Everyone!")
+print(final_str)
+
