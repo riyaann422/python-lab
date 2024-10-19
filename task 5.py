89c80ccdae46cfbecda976941726b75345f2514e
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-10-19 14:50:03 +0530

    Add files via upload

---
+++ task 5.py
@@ -0,0 +1,14 @@
+'''Author:Riya
+Date:19/10/2924
+Description:the program that performs the five basic arithmatic operators
+'''
+
+number1=int(input("Enter  the first number:"))
+number2=int(input("Enter the second number: "))
+number3=int(input("Enter the third number:"))
+print(" The sum of number1 and number2 is :",number1+number2)
+print("The difference when number2  is subtracted number1:",number1-number2)
+print("The product of number1 and number2 is :,",number1*number2)
+print("The quotient when number1 divide by number2 is :",number1/number2)
+print("The remainder when number1 is divide by number2 is:",number1%number2)
+print("The result of (num1 + num2) * num3 / 2 is:", (number1+ number2) * number3 / 2)
