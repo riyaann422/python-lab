5aa239f78dbf6be60f74080a38168ccfe04fdd58
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-10-25 14:30:49 +0530

    Add files via upload

---
+++ no.pytask8.py
@@ -0,0 +1,10 @@
+'''Author:Riya ann mathew
+Date:25-10-2024
+Description: Python program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the number'''
+number1=int(input("Enter the first number"))
+number2=int(input("Enter the second number"))
+if number1>number2:
+    print(number1," is greater than",number2)
+else:
+    print(number2,"is greater than",number1)
+print("The remaining statement")
