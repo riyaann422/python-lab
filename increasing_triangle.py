d7694a07ca71317799b7b00665cd7be9b4dd083a
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2024-11-22 15:20:00 +0530

    Add files via upload

---
+++ increasing_triangle.py
@@ -0,0 +1,31 @@
+limit=int(input("Enter no of rows:"))
+for i in range(1,limit+1):
+    for j in range(i):
+        print('*',end="")
+    print()
+
+
+limit=int(input("Enter no of rows:"))
+for i in range(limit,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+
+
+
+limit=int(input("Enter the number of rows:"))
+for i in range (1,limit+1):
+    for j in range(limit-i):
+        print(' ',end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()
+
+
+limit=int(input("Enter the number of rows:"))
+for i in range(limit+1,0,-1):
+    for j in range(limit-i):
+        print(' ',end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()
